7fe8b8d80e111ced3916ececa46c93c42920b2ee
Adwait Srivastava <studyuse1702@gmail.com>
2024-02-02 16:13:38 -0500

    Initial Commit

---
+++ qhacks-24/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Welcome to the React app! 
+      </h1>
+    </div>
+  );
+}
+
+export default App;
