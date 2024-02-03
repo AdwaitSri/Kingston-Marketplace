c5ffc647c014b5352b282c058a1b244fa5f7f596
Kevin <yaokevin820@gmail.com>
2024-02-03 01:09:44 -0500

    Homepage complete (actual)

---
+++ qhacks-24/src/components/HomeNavbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function HomeNavbar() {
+    return (
+        <div
+            id="homeNavbarContainer"
+            className="h-[70px] font-jose bg-orange-1 flex items-center justify-between"
+        >
+            {/*  maybe add a logo div here that links back to homepage */}
+            <div
+                className="w-[230px] pl-[10px] font-semibold flex items-center justify-between h-[100%]"
+            >
+                <img
+                    src="/images/logo.png"
+                    alt="logo"
+                    className="h-[70%]"
+                />
+
+                <Link 
+                    to="/listings"
+                    className="text-xl"
+                >
+                    Listings
+                </Link>
+                <Link 
+                    to="/about"
+                    className="text-xl"
+                >
+                    About
+                </Link>
+            </div>
+            <div
+                className="flex items-center justify-between mr-[20px] w-[230px]"
+            >
+                <Link 
+                    to="/signup"
+                    className="text-xl w-[110px] text-center bg-black text-white hover:bg-white hover:text-black duration-150 p-[4px] pt-[6px] rounded-full tracking-tight"
+                >
+                    Sign Up
+                </Link>
+                <Link 
+                    to="/login"
+                    className="text-xl w-[110px] text-center bg-black text-white hover:bg-white hover:text-black duration-150 p-[4px] pt-[5.5px] rounded-full tracking-tight"
+                >
+                    Log In
+                </Link>
+            </div>
+
+
+
+
+        </div> 
+    )
+}
---
+++ qhacks-24/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HomeNavbar from "../components/HomeNavbar";
+import ImageGallery from "../components/ImageGallery";
+
+export default function Home() {
+    return (
+        <div
+            id="homeContainer"
+            className=""
+        >
+            <HomeNavbar />
+            <ImageGallery />
+            <div className="justify-center mx-auto w-fit">
+                <button className="font-jose text-2xl font-semibold w-[300px] p-[6px] pt-[10px] mt-10 text-center bg-orange-1 text-black hover:bg-blue-600 hover:text-white duration-150 rounded-full">
+                    Start Shopping Today!
+                </button>
+            </div>
+        </div>
+    )
+}
